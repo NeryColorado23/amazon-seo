d9e3ad9f3c6636a26237d5ff04d428bf5414c547
NeryColorado23 <ncoloradom@miumg.edu.gt>
2026-05-22 15:42:57 -0600

    cambio

--- back end/config/googleSheets.js
+++ back end/config/googleSheets.js
@@ -1,9 +1,23 @@
 const { google } = require('googleapis');
-const path = require('path');
 
 const getSheetData = async (sheetId, range = 'A:I') => {
+  let credentials;
+
+  try {
+    if (process.env.GOOGLE_CREDENTIALS_JSON) {
+      credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON);
+    } else {
+      const path = require('path');
+      const fs = require('fs');
+      const filePath = path.resolve('./google-credentials.json');
+      credentials = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    }
+  } catch (err) {
+    throw new Error(`Error cargando credenciales Google: ${err.message}`);
+  }
+
   const auth = new google.auth.GoogleAuth({
-    keyFile: path.resolve(process.env.GOOGLE_CREDENTIALS_PATH),
+    credentials,
     scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
   });
 
